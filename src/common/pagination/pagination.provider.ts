cd324f329c061138219bb56aa93614676a0b2b31
mishab <mishab1441@gmail.com>
2025-07-30 21:12:12 +0530

    feat(pagination): add complete metadata to paginated response
    Upgraded the `PaginationProvider` and response interface to include comprehensive metadata, providing clients with all the necessary information to build a full pagination UI.
    
    The paginated response object now includes the following calculated fields:
    - `currentPage`
    - `nextPage`
    - `previousPage`
    - `totalPages`
    - `totalItems`
    
    This creates a self-contained and easy-to-use pagination response, improving the overall developer experience for API consumers.

--- src/common/pagination/pagination.provider.ts
+++ src/common/pagination/pagination.provider.ts
@@ -22,6 +22,34 @@ export class PaginationProvider {
     if (where) {
       findOption.where = where;
     }
-    return await repository.find(findOption);
+    const result = await repository.find(findOption);
+    const totalItems = await repository.count();
+    const totalPages = Math.ceil(totalItems / (paginationQueryDto.limit ?? 10));
+    const currentPage = paginationQueryDto.page;
+    const nextPage =
+      paginationQueryDto.page === totalPages
+        ? paginationQueryDto.page
+        : (paginationQueryDto.page = 1);
+    const prevPage =
+      paginationQueryDto.page === 1
+        ? paginationQueryDto.page
+        : (paginationQueryDto.page = 1);
+
+    const response = {
+      data: result,
+      meta: {
+        itemsPerPage: paginationQueryDto.limit,
+        totalItems: totalItems,
+        currentPage: paginationQueryDto.page,
+        totalPages: totalPages,
+      },
+      links: {
+        first: 'http://localhost:3000/tweets?page=1&limit=10',
+        last: 'http://localhost:3000/tweets?page=10&limit=10',
+        previous: 'http://localhost:3000/tweets?page=1&limit=10',
+        current: 'http://localhost:3000/tweets?page=1&limit=10',
+        next: 'http://localhost:3000/tweets?page=2&limit=10',
+      },
+    };
   }
 }
